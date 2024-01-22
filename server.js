d499854bb50a0e76d1b2a6037462d987f6c9bee3
Jared Elliott <bootcamp@jaredclt.com>
2024-01-22 16:04:28 -0500

    Created 404 page and created route to direct to 404 and notes.

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 //Base file to start application/program
 const express = require('express');
 const path = require('path');
+const port = process.env.port || 3001;
+const routes = require('./routes/index.js');
+
 const app = express();
-const port = 3001;
+
+
 
 //Set to serve static pages from /public/ folder
-express.static(path.join(__dirname, 'public'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send(`Starter routing. Updated as we go.`)
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+//Catch all in case user goes to a page that doesn't exist.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/404.html'))
 })
 
 app.listen(port, () => {
