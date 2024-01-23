887a7e3b9f0e650bffb04e90d09f6de1110e90f5
Jared Elliott <bootcamp@jaredclt.com>
2024-01-22 22:42:43 -0500

    Added post functionality & uuid install from npm.

--- routes/notes.js
+++ routes/notes.js
@@ -1,4 +1,5 @@
 const notes = require('express').Router();
+const { v4: uuid } = require('uuid');
 const fs = require('fs');
 
 notes.get('/', (req, res) => {
@@ -14,16 +15,36 @@ notes.post('/', (req,res) => {
     if(!title || !text) { return res.status(400).send(`No data provided in ${req.method}.`); }
 
     console.log(`${req.method} was received.`);
-    console.group(`${title} & ${text}`)
-    console.groupEnd();
+    const newNote = {
+        //Unique ID assigned with uuid() function
+        id: uuid(),
+        title: title,
+        text: text
+    }
     
-    res.send(`${req.method} was received.`)
+    const response = {
+        status: 'success',
+        body: newNote,
+    }
+
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) { console.error(err); return; }
+        const currData = JSON.parse(data);
+        currData.push(newNote);
+
+        fs.writeFile("./db/db.json", JSON.stringify(currData, null, 4), (err) => {
+            err ? console.error(err) : console.log(`Review for ${newReview.product} has been written to JSON file`)
+        })
+    })
+
+    res.status(201).json(response);
+
 });
 
-notes.delete('/:note_id', (req, res) => {
-    if(!req.params.note_id) { res.status(400).send('No ID provided to delete.'); }
-        res.send(`${req.method} was received on id: ${req.params.note_id}`);
+notes.delete('/:id', (req, res) => {
+    if(!req.params.id) { res.status(400).send('No ID provided to delete.'); }
+    
+    res.send(`${req.method} was received on id: ${req.params.note_id}`);
 });
-
 
 module.exports = notes;
--- server.js
+++ server.js
@@ -11,22 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 //Direct /api request to the routes/index.js file for handling
 app.use('/api', api);
 
-//Set to serve static pages from /public/ folder
+//Set to serve static pages from /public folder
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    console.log('N/A')
-})
-
+//Catch going to /notes and direct to the notes.html page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
 //Catch all in case user goes to a page that doesn't exist.
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/404.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 })
 
+//App listening on port provided.
 app.listen(port, () => {
     console.log(`Note Taking app is listening on ${port}.`);
 })
